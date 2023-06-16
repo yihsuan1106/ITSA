a129892aa4b3f5ad1b4878d355ea200060a1314d
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2023-06-16 23:05:07 +0800

    Create C_MM10.cpp

---
+++ C_MM10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    float up, down = 1, high;
+    cin >> up;
+    cout << fixed << setprecision(1) << up*9/5+32 << "\n";
+    return 0;
+}
