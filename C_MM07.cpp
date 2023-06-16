d386693dad2bc79b15b34687d9908fb27c4b48f6
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2023-06-16 22:54:54 +0800

    Create C_MM07.cpp

---
+++ C_MM07.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+  int up, down, high;
+  cin >> up;
+  cout << up << " " << up*up << " " << up*up*up << "\n";
+  return 0;
+}
