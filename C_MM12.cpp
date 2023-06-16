ce076683405dea789af0560d99cb04c385e59999
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2023-06-16 23:30:41 +0800

    Create C_MM12.cpp

---
+++ C_MM12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int up, down = 1, high;
+    cin >> up;
+    cout << ceil(up/0.238) << "\n";
+    return 0;
+}
