d83195eb50d57fc0885dda437f77dc18bcc9a378
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2023-06-16 22:46:12 +0800

    Create C_MM03.cpp

---
+++ C_MM03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int up, down;
+    while(cin >> up >> down){
+        cout<< up+down << "\n";
+    }
+    return 0;
+}
