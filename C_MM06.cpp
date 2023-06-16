861ba055c93e519645b41564c84004d928cfcbcc
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2023-06-16 22:52:24 +0800

    Create C_MM06.cpp

---
+++ C_MM06.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+  float up, down, high;
+  cin >> up;
+  cout << fixed << setprecision(1) << up*1.6 << "\n";
+  return 0;
+}
